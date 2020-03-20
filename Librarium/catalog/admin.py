391148811c1774ed476093b70bfd9cc643a7cba5
Zachary Hubbell <spideyclick@gmail.com>
2020-03-19 19:15:41 -0500

    Admin: Update Author and BookInstance views

--- Librarium/catalog/admin.py
+++ Librarium/catalog/admin.py
@@ -5,7 +5,17 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
 admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
 admin.site.register(Language)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+	list_display = ('book', 'language', 'status', 'due_back')
+	list_filter = ('language', 'status')
