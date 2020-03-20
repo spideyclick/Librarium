55974d36d28051344b24c4a0f2999ca7f3761a9a
spideyclick <spideyclick@gmail.com>
2020-03-20 13:01:57 -0500

    Add list and detail views for Books

--- Librarium/catalog/views.py
+++ Librarium/catalog/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from catalog.models import Book, Author, BookInstance, Genre, Language
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    num_genres = Genre.objects.all().count()
+    num_the_in_title = Book.objects.filter(title__icontains='the').count()
+    
+    # The 'all()' is implied by default.    
+    num_authors = Author.objects.count()
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_the_in_title': num_the_in_title,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    # paginate_by = 2
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
