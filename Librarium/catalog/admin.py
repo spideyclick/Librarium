5b69a3a289b7a30bb38dc80a66dfbdfdbc930221
Zachary Hubbell <spideyclick@gmail.com>
2020-03-19 20:21:58 -0500

    Admin: Added inline view models

--- Librarium/catalog/admin.py
+++ Librarium/catalog/admin.py
@@ -4,18 +4,39 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
+# admin.site.register(Book)
 # admin.site.register(Author)
 admin.site.register(Genre)
 # admin.site.register(BookInstance)
 admin.site.register(Language)
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    list_filter = ('author', 'genre')
+    inlines = [BooksInstanceInline]
+
+class AuthorInstanceInline(admin.TabularInline):
+    model = Book
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-	list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [AuthorInstanceInline]
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-	list_display = ('book', 'language', 'status', 'due_back')
-	list_filter = ('language', 'status')
+    list_display = ('book', 'language', 'status', 'due_back')
+    list_filter = ('language', 'status')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'language', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
