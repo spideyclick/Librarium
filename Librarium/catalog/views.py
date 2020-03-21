b0dbb198e254b301f5263216742ffe7bd415fafa
spideyclick <spideyclick@gmail.com>
2020-03-21 11:39:28 -0500

    Create basic user authentication

--- Librarium/catalog/views.py
+++ Librarium/catalog/views.py
@@ -20,6 +20,10 @@ def index(request):
     # The 'all()' is implied by default.    
     num_authors = Author.objects.count()
     
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -27,6 +31,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'num_the_in_title': num_the_in_title,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
