b3b3aeba72dc2a8af8629f925d400661603a2055
spideyclick <spideyclick@gmail.com>
2020-03-23 22:13:24 -0500

    Add User-based Author manipulation

--- Librarium/catalog/urls.py
+++ Librarium/catalog/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
 	path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 	path('loanedbooks/', views.LoanedBooksAll.as_view(), name='all-borrowed'),
 	path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+	path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
 ]
--- Librarium/catalog/views.py
+++ Librarium/catalog/views.py
@@ -7,9 +7,12 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse
+from django.urls import reverse_lazy
 
 from catalog.models import Book, Author, BookInstance, Genre, Language
+from catalog.models import Author
 from catalog.forms import RenewBookForm
 
 # Create your views here.
@@ -112,3 +115,19 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    # initial = {'date_of_death': '05/01/2018'}
+
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
