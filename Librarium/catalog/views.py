171c71cb3e09fc791e325c050d067e27d04dd7fd
spideyclick <spideyclick@gmail.com>
2020-03-24 19:38:01 -0500

    Require permission on Author and Book manipulation

--- Librarium/catalog/views.py
+++ Librarium/catalog/views.py
@@ -116,27 +116,39 @@ def renew_book_librarian(request, pk):
 
     return render(request, 'catalog/book_renew_librarian.html', context)
 
-class AuthorCreate(CreateView):
+
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    permission_required = ('catalog.can_mark_returned')
     model = Author
     fields = '__all__'
     # initial = {'date_of_death': '05/01/2018'}
 
-class AuthorUpdate(UpdateView):
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = ('catalog.can_mark_returned')
     model = Author
     fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
 
-class AuthorDelete(DeleteView):
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = ('catalog.can_mark_returned')
     model = Author
     success_url = reverse_lazy('authors')
 
-class BookCreate(CreateView):
+
+class BookCreate(PermissionRequiredMixin, CreateView):
+    permission_required = ('catalog.can_mark_returned')
     model = Book
     fields = '__all__'
 
-class BookUpdate(UpdateView):
+
+class BookUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = ('catalog.can_mark_returned')
     model = Book
     fields = '__all__'
 
-class BookDelete(DeleteView):
+
+class BookDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = ('catalog.can_mark_returned')
     model = Book
     success_url = reverse_lazy('books')
